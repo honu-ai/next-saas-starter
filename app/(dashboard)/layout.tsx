f51ae8ae6073b985413b56f97eba2996a17c8b47
Velizar Mihaylov <veli.mihaylov@honu.ai>
2025-06-11 15:15:46 +0100

    Add product flag support.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -8,6 +8,7 @@ import { signOut } from '@/app/(login)/actions';
 import { useRouter } from 'next/navigation';
 import Navbar from '@/components/nav-bar';
 import UserAvatarMenu from '@/components/user-avatar-menu';
+import content from '@/content.json';
 
 function Header() {
   const { userPromise } = useUser();
@@ -25,29 +26,34 @@ function Header() {
       <div className='mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8'>
         <Navbar
           links={[
-            { label: 'Features', path: '#benefits' },
-            { label: 'How it works', path: '#solution' },
-            { label: 'FAQ', path: '#faq' },
+            { label: 'Features', path: '/#benefits' },
+            { label: 'How it works', path: '/#solution' },
+            { label: 'FAQ', path: '/#faq' },
+            ...(content?.metadata?.product
+              ? [{ label: 'Pricing', path: '/pricing' }]
+              : []),
           ]}
         >
-          {/* {user ? (
-            <UserAvatarMenu user={user} handleSignOut={handleSignOut} />
-          ) : (
-            <div className='flex items-center space-x-3'>
-              <Button
-                asChild
-                className='rounded-full border border-black bg-transparent px-5 py-2 text-sm font-medium text-black hover:bg-gray-100 dark:border-white dark:text-white dark:hover:bg-gray-800'
-              >
-                <Link href='/sign-in'>Log In</Link>
-              </Button>
-              <Button
-                asChild
-                className='rounded-full bg-black px-5 py-2 text-sm font-medium text-white hover:bg-gray-800'
-              >
-                <Link href='/sign-up'>Sign Up</Link>
-              </Button>
-            </div>
-          )} */}
+          {content.metadata.product ? (
+            user ? (
+              <UserAvatarMenu user={user} handleSignOut={handleSignOut} />
+            ) : (
+              <div className='flex items-center space-x-3'>
+                <Button
+                  asChild
+                  className='rounded-full border border-black bg-transparent px-5 py-2 text-sm font-medium text-black hover:bg-gray-100 dark:border-white dark:text-white dark:hover:bg-gray-800'
+                >
+                  <Link href='/sign-in'>Log In</Link>
+                </Button>
+                <Button
+                  asChild
+                  className='rounded-full bg-black px-5 py-2 text-sm font-medium text-white hover:bg-gray-800'
+                >
+                  <Link href='/sign-up'>Sign Up</Link>
+                </Button>
+              </div>
+            )
+          ) : null}
         </Navbar>
       </div>
     </header>
