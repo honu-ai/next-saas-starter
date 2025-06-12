ab665f2d442227174bcb4bc07d1c4f8567d29eaa
gonzalogacc <gonzalogacc@gmail.com>
2025-06-12 14:39:59 +0200

    Use openssl as suggested in docs

--- setup_secrets.sh
+++ setup_secrets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SECRET=$(grep STRIPE_API_KEY .env | cut -d '=' -f 2)
 echo STRIPE_WEBHOOK_SECRET=`docker run stripe/stripe-cli --api-key=${SECRET} listen --print-secret --forward-to localhost:3001/api/stripe/webhook` >> .env
-echo AUTH_SECRET=`uuidgen | sed 's/[-]//g' | head -c 20` >> .env
+echo AUTH_SECRET=`openssl rand -base64 32` >> .env
